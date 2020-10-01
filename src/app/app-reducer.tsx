cdb7bff0f70f4cc7d1a0e46b185e25454f957006
Prough123 <slipok1999@mail.ru>
2020-10-01 23:43:10 +0300

    Error handling , addition loadings

---
+++ src/app/app-reducer.tsx
@@ -0,0 +1,32 @@
+import {Simulate} from "react-dom/test-utils";
+
+export type RequestStatusType = 'idle' | 'loading' | 'succeeded' | 'failed'
+
+
+const initialState = {
+    status: 'idle' as RequestStatusType,
+    error: null as string | null
+}
+
+type InitialStateType = typeof initialState
+
+export const appReducer = (state: InitialStateType = initialState, action: ActionsType): InitialStateType => {
+    switch (action.type) {
+        case 'APP/SET-STATUS':
+            return {...state, status: action.status}
+        case "APP/SET-ERROR":
+            return {...state, error: action.error}
+        default:
+            return state
+    }
+}
+
+export const setAppStatusAC = (status: RequestStatusType) => ({type: 'APP/SET-STATUS', status} as const)
+
+export const setAppErrorAC = (error:string | null) => ({type:'APP/SET-ERROR', error} as const)
+
+export type SetAppStatusACType = ReturnType<typeof setAppStatusAC>
+
+export type setAppErrorACType = ReturnType<typeof setAppErrorAC>
+
+type ActionsType = SetAppStatusACType | setAppErrorACType
