64f501b290b8b40be947e9c40693b1996de44909
Prough123 <slipok1999@mail.ru>
2020-10-10 00:57:27 +0300

    Refactoring

---
+++ src/app/app-reducer.ts
@@ -0,0 +1,56 @@
+import {setIsLoggedInAC} from "../features/Login/auth-reducer";
+import {Dispatch} from "redux";
+import {authAPI} from "../api/todolists-api";
+
+const initialState: InitialStateType = {
+    status: 'idle',
+    error: null,
+    isInitialized: false
+}
+
+export const appReducer = (state: InitialStateType = initialState, action: ActionsType): InitialStateType => {
+    switch (action.type) {
+        case 'APP/SET-IS-INIT':
+            return {...state, isInitialized:true}
+        case 'APP/SET-STATUS':
+            return {...state, status: action.status}
+        case 'APP/SET-ERROR':
+            return {...state, error: action.error}
+        default:
+            return {...state}
+    }
+}
+
+export type RequestStatusType = 'idle' | 'loading' | 'succeeded' | 'failed'
+export type InitialStateType = {
+    // происходит ли сейчас взаимодействие с сервером
+    status: RequestStatusType
+    // если ошибка какая-то глобальная произойдёт - мы запишем текст ошибки сюда
+    error: string | null
+    isInitialized: boolean
+}
+
+export const setAppErrorAC = (error: string | null) => ({type: 'APP/SET-ERROR', error} as const)
+export const setAppStatusAC = (status: RequestStatusType) => ({type: 'APP/SET-STATUS', status} as const)
+export const setIsInitializedAC = () => ({type: 'APP/SET-IS-INIT'} as const)
+
+export const initializeAppTC = () => (dispatch: Dispatch) => {
+    authAPI.me().then(res => {
+        if (res.data.resultCode === 0) {
+            dispatch(setIsLoggedInAC(true));
+        } else {
+        }
+
+        dispatch(setIsInitializedAC())
+    })
+}
+
+
+export type SetAppErrorActionType = ReturnType<typeof setAppErrorAC>
+export type SetAppStatusActionType = ReturnType<typeof setAppStatusAC>
+export type setIsInitializedACType = ReturnType<typeof setIsInitializedAC>
+
+type ActionsType =
+    | SetAppErrorActionType
+    | SetAppStatusActionType
+    | setIsInitializedACType
