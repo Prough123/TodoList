8ac20fdc4811c43e1b92b28f1b45cf5352fc0c29
Prough123 <slipok1999@mail.ru>
2021-01-31 19:52:33 +0300

    tests refactoring

--- src/app/app-reducer.test.ts
+++ src/app/app-reducer.test.ts
@@ -6,19 +6,19 @@ beforeEach(() => {
     startState = {
         error: null,
         status: 'idle'
-    }
+    } as  InitialStateType
 })
 
 test('correct error message should be set', () => {
 
-    const endState = appReducer(startState, setAppErrorAC('some error'))
+    const endState = appReducer(startState, setAppErrorAC({error:'some error'}))
 
     expect(endState.error).toBe('some error');
 })
 
 test('correct status should be set', () => {
 
-    const endState = appReducer(startState, setAppStatusAC('loading'))
+    const endState = appReducer(startState, setAppStatusAC({status:'loading'}))
 
     expect(endState.status).toBe('loading');
 })
